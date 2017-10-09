60048e48fdff89f9b9b40b643c812fbe816a0f33
ChristopheBossens <christophe.bossens@gmail.com>
2017-10-09 22:39:06 +0200

    Updated python code

--- Gratings/python/grating_generator.py
+++ Gratings/python/grating_generator.py
@@ -5,39 +5,67 @@ Created on Thu Jan 21 13:02:13 2016
 @author: Christophe
 """
 import math
-import numpy
+import numpy as np
 
-# Converts a physical distance into a measure of visual degrees
-def metric2vd(metricStimulusSize, viewDistance):
-    vd = 2.0*math.degrees(math.atan( metricStimulusSize/(2.0*viewDistance)))
+# Gives the size of the stimulus expressed in visual degrees, if you
+# now the size and viewing distance in physical units (e.g. mm)
+def metric2vd(stim_size_metric, viewing_distance):
+    vd = 2.0*math.degrees(math.atan( stim_size_metric/(2.0*viewing_distance)))
     return vd
 
-# Converts a coordinate plane into units of visual degrees
-def scale2degree(*arg):
-    if len(arg) == 2:
-        xViewAngle = arg[0]
-        xScreenPixels = arg[1]
-        yViewAngle = xViewAngle
-        yScreenPixels = xScreenPixels
-    elif len(arg) == 4:
-        xViewAngle = arg[0]
-        yViewAngle = arg[1]
-        xScreenPixels = arg[2]
-        yScreenPixels = arg[3]
+# Create a grating stimulus of 
+def create_grating(W,H, stim_orientation, cpp, contrast = 1.0, phase_offset = 0):
+    # Convert parameters to radians
+    orientation = stim_orientation*math.pi/180.0 
+    phase_offset = phase_offset*math.pi/180.0
+    
+    if W%2 == 0:
+        x_range = np.arange(-W/2,W/2,1)
     else:
-        return -1
+        x_range = np.arange(-(W-1)/2, (W+1)/2,1)
+    
+    if H%2 == 0:
+        y_range = np.arange(-H/2, H/2, 1)
+    else:
+        y_range = np.arange(-(H-1)/2, (H+1)/2,1)
         
-    xDegreesPerPixel = xViewAngle/xScreenPixels
-    yDegreesPerPixel = yViewAngle/yScreenPixels
+    [X,Y] = np.meshgrid(x_range,y_range)
+    xRotated = np.cos(orientation)*X + np.sin(orientation)*Y
     
-    xPixelScale = [ (xDegreesPerPixel*x) - (xViewAngle/2) for x in range(0,xScreenPixels)]
-    yPixelScale = [ (yDegreesPerPixel*y) - (yViewAngle/2) for y in range(0,yScreenPixels)]
-    
-    [x,y] = numpy.meshgrid(xPixelScale, yPixelScale)
-    return x,y
-
-def CreateGrating(X,Y, stimulusOrientation, cyclesPerDegree):
-    orientation = stimulusOrientation*math.pi/180.0
-    xRotated = numpy.cos(orientation)*X + numpy.sin(orientation)*Y
-    grating = numpy.cos( 2.0*math.pi*cyclesPerDegree*xRotated)
+    grating = contrast * np.cos( 2.0*math.pi*cpp*xRotated + phase_offset)
     return grating
+
+def create_circular_mask(W,H,radius):
+    if W%2 == 0:
+        x_range = np.arange(-W/2,W/2,1)
+    else:
+        x_range = np.arange(-(W-1)/2, (W+1)/2,1)
+    
+    if H%2 == 0:
+        y_range = np.arange(-H/2, H/2, 1)
+    else:
+        y_range = np.arange(-(H-1)/2, (H+1)/2,1)
+        
+    [X,Y] = np.meshgrid(x_range,y_range)
+    
+    distances = np.sqrt(np.square(X) + np.square(Y))
+    mask = np.zeros(distances.shape)
+    mask[distances <= float(radius)] = 1
+    return mask
+
+def create_gaussian_mask(W, H, sigma):
+    if W%2 == 0:
+        x_range = np.arange(-W/2,W/2,1)
+    else:
+        x_range = np.arange(-(W-1)/2, (W+1)/2,1)
+    
+    if H%2 == 0:
+        y_range = np.arange(-H/2, H/2, 1)
+    else:
+        y_range = np.arange(-(H-1)/2, (H+1)/2,1)
+        
+    [X,Y] = np.meshgrid(x_range,y_range)
+    
+    mask = np.exp(- ( (np.square(X)/(2*float(sigma)**2) + np.square(Y)/(2*float(sigma)**2))))
+    
+    return mask
--- Gratings/python/gratings_demo.py
+++ Gratings/python/gratings_demo.py
@@ -4,25 +4,39 @@ Created on Thu Jan 21 13:02:20 2016
 
 @author: Christophe
 """
-from grating_generator import *
+import grating_generator as gg
 import matplotlib.pyplot as plt
 from PIL import Image
+import numpy as np
 
-pixelStimulusSize = 240 
-metricStimulusSize = 75
-viewDistance = 100
-stimulusOrientation = 0
-cyclesPerDegree = 0.5
+stim_size_pixel   = 128 # pixels
+stim_size_metric  = 75  # mm
+viewing_distance  = 100 # mm
+stim_orientation  = 0   # degrees
+cycles_per_degree = 0.3   
 
-degreesStimulusSize = metric2vd(metricStimulusSize,viewDistance)
-[X,Y] = scale2degree(degreesStimulusSize, pixelStimulusSize)
+# The following code can be used to convert cycles per degree into cycles per pixel
+stim_size_degrees = gg.metric2vd(stim_size_metric, viewing_distance)
+cycles_per_pixel = cycles_per_degree*stim_size_degrees/stim_size_pixel
 
-gratingStimulus = CreateGrating(X,Y,stimulusOrientation,cyclesPerDegree)
-plt.imshow(gratingStimulus, cmap = 'Greys')
+
+# Create a grating and a gaussian and circular mask
+grating = gg.create_grating(stim_size_pixel,stim_size_pixel,stim_orientation,cycles_per_pixel,0.5)
+mask_circular = gg.create_circular_mask(stim_size_pixel,stim_size_pixel,63)
+mask_gauss = gg.create_gaussian_mask(stim_size_pixel,stim_size_pixel,20)
+
+grating_circular = grating*mask_circular
+grating_gauss    = grating*mask_gauss
+
+# Plot the images
+plt.subplot(1,2,1)
+plt.imshow(grating*mask_circular, cmap='Greys', clim=(-1.0, 1.0))
+plt.subplot(1,2,2)
+plt.imshow(grating*mask_gauss, cmap='Greys', clim=(-1.0, 1.0))
 
 # Note: the previous image has values in the range [-1 1]. Typical computer images 
 # are saved with integer values in the range [0..255]. This means we need to do
 # a conversion first
-gratingScaled = (gratingStimulus+1.0)/2.0
-im = Image.fromarray( (gratingScaled*255).astype(numpy.uint8))
-im.save("stim1.bmp")
+grating_scaled = 255*(grating_gauss+1.0)/2.0
+im = Image.fromarray( grating_scaled.astype(np.uint8))
+im.save("grating_gauss.bmp")
